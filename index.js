c707e412ac167a77af0268bfadd51cf0cf2a49b7
John G <john.guckian13@gmail.com>
2025-06-29 17:59:39 -0700

    changes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 const port = 3000;
 app.use(express.static('public'));
 
+const apiKey = process.env.WEATHER_API_KEY;
 
 function capitalizeWords(str) {
     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
