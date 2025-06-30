636e2c267b7ba3134484f7e7b8bb5a94b4b6737e
John G <john.guckian13@gmail.com>
2025-06-29 17:15:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+const port = 3000;
+app.use(express.static('public'));
+
+const apiKey = "f348829cc54363d0c6c8a3180cac23c1";
+
+function capitalizeWords(str) {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/weather', async (req, res) => {
+    const cityName = req.body['city-input'];
+    try {
+        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
+        const latitude = response.data[0].lat;
+        const longitude = response.data[0].lon;
+        const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+        const description = capitalizeWords(weatherResponse.data.weather[0].description);
+        const temperature = weatherResponse.data.main.temp;
+        res.render('weather.ejs', { city: cityName, weather: description, temp: temperature});
+    } catch (error) {
+        console.error(error.message);
+    }
+   
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
